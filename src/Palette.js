73dad935efd66f7bdac4fbe8a8e7db0fcdd6a1a9
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-03-29 05:00:45 -0400

    added ability to copy rgb,rgba and hex colors

--- src/Palette.js
+++ src/Palette.js
@@ -10,23 +10,32 @@ export default class Palette extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      level: 500
+      level: 500,
+      format: 'hex'
     };
 
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
   changeLevel(level) {
     this.setState({ level });
   }
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
   render() {
     const { colors } = this.props.pallete;
-    const { level } = this.state;
+    const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} />
     ));
     return (
       <div className='Pallete'>
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
         <div className='Pallete-colors'>{colorBoxes}</div>
       </div>
     );
