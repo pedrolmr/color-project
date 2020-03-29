5366b2ed941223fec082f60c72dd6d0d27482e1f
pedrolmr <pedroluismontesinosrivera@gmail.com>
2020-03-29 00:12:24 -0400

    added color slider

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import seedColors from './seedColors';
 import { generatePalette } from './ColorHelpers';
 
 function App() {
-  console.log(generatePalette(seedColors[4]));
   return (
     <div>
-      <Palette {...seedColors[4]} />
+      <Palette pallete={generatePalette(seedColors[4])} />
     </div>
   );
 }
